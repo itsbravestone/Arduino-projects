192a12ea47e553679919bddba49dc941e262da7b
Steve <40924023+itsbravestone@users.noreply.github.com>
2021-08-03 21:30:16 +0530

    Update WiFi_Wx_Station.ino

--- WiFi Weather Station/WiFi_Wx_Station.ino
+++ WiFi Weather Station/WiFi_Wx_Station.ino
@@ -16,3 +16,21 @@ int DEBUG = 0;      // DEBUG counter; if set to 1, will write values back via se
 #define CC3000_CS       10  // Preferred is pin 10 on Uno
 #define DHTPIN          3   // DHT 22  (AM2302)
                             // The UV sensor and Barometer are on i2C pins
+
+// Constants
+#define DHTTYPE DHT22                                  // DHT 22 (AM2302)
+char SERVER[] = "rtupdate.wunderground.com";           // Realtime update server - RapidFire
+//char SERVER [] = "weatherstation.wunderground.com";  //standard server
+char WEBPAGE [] = "GET /weatherstation/updateweatherstation.php?";
+char ID [] = "xxxx";
+char PASSWORD [] = "xxxx";
+
+// Global Variables
+SFE_CC3000 wifi = SFE_CC3000(CC3000_INT, CC3000_EN, CC3000_CS);
+SFE_CC3000_Client client = SFE_CC3000_Client(wifi);
+//RTC_DS1307 rtc;                       // Hardware RTC time -- If used
+DHT dht(DHTPIN, DHTTYPE);               // DHT 22  (AM2302)
+Adafruit_BMP085 bmp;                    // BMP Pressure Sensor
+Adafruit_SI1145 uv = Adafruit_SI1145(); // UV Sensor
+unsigned int connections = 0;           // number of connections
+unsigned int timeout = 30000;           // Milliseconds -- 1000 = 1 Second
