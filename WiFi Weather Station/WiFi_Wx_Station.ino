1a9b199aac0b55f28ef0d8ad9a72fa06b92ffa46
Steve <40924023+itsbravestone@users.noreply.github.com>
2021-08-03 21:29:02 +0530

    Update WiFi_Wx_Station.ino

--- WiFi Weather Station/WiFi_Wx_Station.ino
+++ WiFi Weather Station/WiFi_Wx_Station.ino
@@ -7,3 +7,12 @@ int DEBUG = 0;      // DEBUG counter; if set to 1, will write values back via se
 #include "DHT.h"                //Humidity
 #include <Adafruit_BMP085.h>    //Barometer
 #include "Adafruit_SI1145.h"    //UV sensor
+#include "RTClib.h" 
+#include <io.h> 
+
+// Pins
+#define CC3000_INT      2   // Needs to be an interrupt pin (D2/D3)
+#define CC3000_EN       7   // Can be any digital pin
+#define CC3000_CS       10  // Preferred is pin 10 on Uno
+#define DHTPIN          3   // DHT 22  (AM2302)
+                            // The UV sensor and Barometer are on i2C pins
